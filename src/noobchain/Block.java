86705fedf6bf591e574f907e3900d97c0011736c
Drumont Erick <drumont06@gmail.com>
2020-10-29 03:19:37 +0100

    initial commit

---
+++ src/noobchain/Block.java
@@ -0,0 +1,50 @@
+package noobchain;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+
+    // noobchain.Block constructor
+    public Block(String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        return StringUtil.applySha256(previousHash+Long.toString(timeStamp)+Integer.toString(nonce)+data);
+    }
+
+    public void mineBlock(int difficulty){
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("noobchain.Block Mined!!! : " + hash);
+    }
+
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("noobchain.Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("noobchain.Transaction Successfully added to noobchain.Block");
+        return true;
+    }
+}
